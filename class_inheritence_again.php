4da5193de41920ed09ea0608a18f748f4d922dc2
Tausif_19 <bahlul.tausif@gmail.com>
2017-07-19 16:49:20 -0700

    fundamental of oop

---
+++ class_inheritence_again.php
@@ -0,0 +1,28 @@
+<?php
+
+class School
+{
+    var $room_no = 90;
+    function rooms()
+    {
+        echo "There is about ".$this->room_no ." in school"; 
+    }
+}
+
+$scl = new School();
+
+class College extends School
+{
+    
+}
+
+$clg = new College();
+
+ echo $clg->room_no;
+ 
+ echo '<br/>'; 
+ 
+ $clg ->rooms();
+
+
+?>
